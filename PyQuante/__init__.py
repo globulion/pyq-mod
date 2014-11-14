194e0e9df3c7322df0dacc76816103b54eec4c49
globulion <blasiak.bartosz@gmail.com>
2014-11-14 16:31:07 +0900

    Version information uploaded and modifications described

--- PyQuante/__init__.py
+++ PyQuante/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """\
 PyQuante: Python Quantum Chemistry
  PyQuante is a toolkit for building quantum chemistry programs in the
@@ -25,7 +26,24 @@ PyQuante: Python Quantum Chemistry
  PyQuante version 1.2 and later is covered by the modified BSD
  license. Please see the file LICENSE that is part of this
  distribution. 
+
+Modifications: mgr inż. Bartosz Błasiak Globulion
+               Wrocław University of Technology
+               email: blasiak.bartosz@gmail.com
+
+List of modifications:
+ o molecular multipole 1-electron integrals
+ o overlap and kinetic 1-electron integral derivatives wrt nuclear coordinates
+ o adding 6-311++G**/6D basis set for H, N, C, O, F, S and Na elements
+ o modification of orbital shell structure for d and f shells
+ o extension of PyQuante.Molecule class
+ o extension of PyQuante.BasisSet class
+ o hybrid double-molecular 2-electron J and K integrals
+
+                                           Last Revision: 14 Nov 2014
 """
+
+__version__ = '1.0.1-BBG'
 
 # Starting to think about making modules available from main-level
 # import. These are good candidates
